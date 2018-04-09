6afde82f3814eb1f3e5b9a9df33c541a9c8eec62
RoyJH <royjh@okstate.edu>
2018-04-08 22:44:15 -0500

    started assignment

---
+++ car.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class newCar {
+	public:
+		string make;
+		string model;
+		int year;
+		void print(){
+			cout<< year << " "<< make<<" "<<model<< endl;
+		}
+}
+int main (){
+cout << "Enter the vehicle's Manufacturer: ";
+cin >> car.make;
+
+cout << "Enter the Model of the vehicle: ";
+cin >> car.model;
+
+cout << "Enter the year of the vehicle: ";
+cin >> car.year;
+
+	
+return 0;
+}
