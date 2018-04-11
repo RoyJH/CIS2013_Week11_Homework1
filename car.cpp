154237c46d141ff01dff20e72a8dc7a1a57a2bc4
RoyJH <royjh@okstate.edu>
2018-04-11 17:12:52 -0500

    Almost done
    Problem printing attributes and adding speed pointers

--- car.cpp
+++ car.cpp
@@ -7,7 +7,7 @@ using namespace std;
 	bool running;
 	bool open =1;
 	string action;
-
+ 
 class NewCar {
 	public:
 		string make;
@@ -18,6 +18,7 @@ class NewCar {
 			cout<< year << " "<< make<<" "<<model<< endl;
 		}
 };
+
 
 int main (){
 	NewCar car;
@@ -30,7 +31,18 @@ int main (){
 
 	cout << "Enter the year of the vehicle: ";
 	cin >> car.year;
-
+		 cout << "_______AVAILABLE ACTIONS_______" << endl;
+		 cout << "change		: Change vehicle." << endl;
+		cout << "drive		: Drive the vehicle." << endl;
+		cout << "start		: Start the vehicle's engine." << endl;
+		cout << "off 		: Turn off the engine." << endl;
+		cout << "stop		: Stop the vehicle (speed must be <= 10)." << endl;
+		cout << "turn left 	: Turn the vehicle left." << endl;
+		cout << "turn right	: Turn the vehicle right." << endl;
+		cout <<  "accel		: Speed up/Accelerate." << endl;
+		cout << "decel		: Slow Down/Decelerate." << endl;
+		cout <<  "print		: Prints the vehicle's attributes." << endl;
+		cout << "exit 		: Ends the program." << endl;
 	cout << "Your selected vehicle is a: " ;
 	car.print();
 	cout << endl;
@@ -44,58 +56,95 @@ int main (){
 	return 0;
 }
 
-void inputs(int&){
+void inputs(){
 		NewCar car;
 	if (action == "drive" ){
+		if (running==false){
+			cout << "Car must be running in order to drive.\n";
+		}
+		else{
 		driving=true;
 		car.speed=15;
-		int car.speed;
+		//car.*speed;
 		cout<< "you begin driving and accelerate to 15mph.\n";
+		}
 	}
 	else if (action == "start"){
+		if (running==true){cout<< "Vehicle is already running.\n";}
+		else{
 		running=true;
-		cout << "you start the vehicle.\n";
+		cout << "you start the vehicle.\n";}
 	}
 	else if (action == "off"){
+		if (car.speed != 0){cout<<"Vehicle must be stationary to turn off engine.\n";}
+		else{
 		running=false;
-		cout<< "the engine is currently off.\n";
+		driving=false;
+		cout<< "the engine is currently off.\n";}
 	}
 		else if (action == "stop"){
+			if(car.speed<10){"You're moving too fast to stop. Reduce speed to 10mph or less.\n";}
+			else{
 		car.speed=0;
-		cout << "you come to a complete stop.\n";
+		driving =false;
+			cout << "you come to a complete stop.\n";}
 	}
 	else if (action == "change"){
-			cout << "Which attribute(s) would you like to change?\n";
-			
+		if (driving == true){cout<< "You can't change vehicles while driving.\n";}
+			cout << "Enter the vehicle's Manufacturer: ";
+			cin >> car.make;
+			cout << "Enter the Model of the vehicle: ";
+			cin >> car.model;
+			cout << "Enter the year of the vehicle: ";
+			cin >> car.year;;
 	}
 	else if (action =="print"){
 		car.print();
 		cout<<endl;
 	}
 	else if (action =="accel"){
+		if (driving==false){cout<< "You must be driving to speed up.\n";}
+		else{
 		car.speed+5;
-		cout<< "you accelerate, current speed is " << car.speed<<endl;
+		cout<< "you accelerate, current speed is " << car.speed<<endl;}
 	}
 	else if (action =="decel"){
+		if (driving==false){cout<< "You must be driving to slow down.\n";}
+		else{
 		car.speed=(car.speed-5);
-		cout<< "you decelerate, current speed is " << car.speed<<endl;
-	}	else if (action =="turn left"){
-		cout << "you take a left turn.\n";
+		cout<< "you decelerate, current speed is " << car.speed<<endl;}
+	}	
+	else if (action =="turn left"){
+		if (driving==false){cout<< "You must be driving in order to turn.\n";}
+		else{		
+		cout << "you take a left turn.\n";}
 	}
 	else if (action == "turn right"){
-		cout << "you take a right turn.\n";
+		if (driving==false){cout<< "You must be driving in order to turn.\n";}
+		else{
+		cout << "you take a right turn.\n";}
 	}
 	
 	else if (action == "exit"){
-		exit(1);
+		open=0;
 	}
-	// else if (action = ""){
+	 else if (action == "list"){
+		 cout << "_______AVAILABLE ACTIONS_______" << endl;
+		 cout << "change		: Change vehicle." << endl;
+		cout << "drive		: Drive the vehicle." << endl;
+		cout << "start		: Start the vehicle's engine." << endl;
+		cout << "off 		: Turn off the engine." << endl;
+		cout << "stop		: Stop the vehicle (speed must be <= 10)." << endl;
+		cout << "turn left 	: Turn the vehicle left." << endl;
+		cout << "turn right	: Turn the vehicle right." << endl;
+		cout <<  "accel		: Speed up/Accelerate." << endl;
+		cout << "decel		: Slow Down/Decelerate." << endl;
+		cout <<  "print		: Prints the vehicle's attributes." << endl;
+		cout << "exit 		: Ends the program." << endl;
 		
-	// }
-	// else if (action = ""){
-	// }
+	 }
 	else {
-		cout << "Invalid action, try again.  Type list to display options\n";
+		cout << "Invalid action, try again.  Type list to display options.\n";
 		cin >> action;
 	}
 }
